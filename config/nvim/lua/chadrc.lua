664c2a1e2d6a0df4e25f53cdc9a3d653d494c0fc
CaioSimioni <83130766+CaioSimioni@users.noreply.github.com>
2024-03-21 11:28:42 -0300

    update chadrc.lua

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -2,8 +2,9 @@
 local M = {}
 
 M.ui = {
+  theme_toggle = {"material-darker"},
   theme = "material-darker",
-
+  transparency = true,
   -- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
